d5d50da1264677dceee6232e437dbc2c6dc40f41
Nadon <jnadon7@gmail.com>
2017-11-17 19:17:17 -0800

    javadoc methods test

--- Util/src/main/java/StringHelper.java
+++ Util/src/main/java/StringHelper.java
@@ -5,59 +5,122 @@ import java.math.BigDecimal;
  *
  *   StringHelper Class
  */
+
 public class StringHelper {
 
-
+    /**
+     *
+     * @param toSearchIn
+     * @param toSearchFor
+     * @param nChars
+     * @param trimResult
+     * @return
+     */
     public static String rightOf(String toSearchIn, String toSearchFor, int nChars, boolean trimResult) {
 
         return null;
     }
 
+    /**
+     * Overloaded method with default param(s):
+     *     nChars=0
+     *     trimResult=true
+     */
     public static String rightOf(String toSearchIn, String toSearchFor) {
-        return rightOf(toSearchIn, toSearchFor, 0, false);
+        return rightOf(toSearchIn, toSearchFor, 0, true);
     }
 
+    /**
+     * Overloaded method with default param(s):
+     *     trimResult=true
+     */
     public static String rightOf(String toSearchIn, String toSearchFor, int nChars){
-        return rightOf(toSearchIn, toSearchFor, nChars, false);
+        return rightOf(toSearchIn, toSearchFor, nChars, true);
     }
 
+    /**
+     * Overloaded method with default param(s):
+     *     default nChars=0
+     */
     public static String rightOf(String toSearchIn, String toSearchFor, boolean trimResult){
-        return rightOf(toSearchIn, toSearchFor, 0, false    );
+        return rightOf(toSearchIn, toSearchFor, 0, trimResult    );
     }
 
+    /**
+     *
+     * @param toSearchIn
+     * @param toSearchFor
+     * @param nChars
+     * @param trimResult
+     * @return
+     */
     public static String leftOf(String toSearchIn, String toSearchFor, int nChars, boolean trimResult) {
-
         return null;
     }
 
+    /**
+     * Overloaded method with default param(s):
+     *     nChars=0
+     *     trimResult=true
+     */
     public static String leftOf(String toSearchIn, String toSearchFor) {
-        return rightOf(toSearchIn, toSearchFor, 0, false);
+        return rightOf(toSearchIn, toSearchFor, 0, true);
     }
 
+    /**
+     * Overloaded method with default param(s):
+     *     trimResult=true
+     */
     public static String leftOf(String toSearchIn, String toSearchFor, int nChars){
-        return rightOf(toSearchIn, toSearchFor, nChars, false);
+        return rightOf(toSearchIn, toSearchFor, nChars, true);
     }
 
+    /**
+     * Overloaded method with default param(s):
+     *     nChars=0
+     */
     public static String leftOf(String toSearchIn, String toSearchFor, boolean trimResult){
-        return rightOf(toSearchIn, toSearchFor, 0, false);
+        return rightOf(toSearchIn, toSearchFor, 0, trimResult);
     }
 
-
-    public static String between(String toSaerchIn, String leftLimit, String rightLimit, boolean trimResults) {
+    /**
+     *
+     * @param toSaerchIn
+     * @param leftLimit
+     * @param rightLimit
+     * @param trimResult
+     * @return
+     */
+    public static String between(String toSaerchIn, String leftLimit, String rightLimit, boolean trimResult) {
         return null;
     }
 
+    /**
+     * Overloaded method with default param(s):
+     *     trimResult=0
+     */
     public static String between(String toSaerchIn, String leftLimit, String rightLimit) {
         return between(toSaerchIn, leftLimit, rightLimit, true);
     }
 
+    /**
+     *
+     * @param phoneNumber
+     * @return
+     */
     public static String getFlatPhoneNumber(String phoneNumber){
         return null;
     }
 
+    /**
+     *
+     * @param phoneNumber
+     * @return
+     */
     public static String getFormatedPhoneNumber(String phoneNumber){
         return null;
     }
+
 
     /*
     @TODO Find a Java alternative for Xelement
