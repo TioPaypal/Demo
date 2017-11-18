9d86688c02bd6948643c9a0887b13f672bf8d332
Nadon <jnadon7@gmail.com>
2017-11-17 19:03:05 -0800

    method and unit test stubs.

--- Util/src/main/java/StringHelper.java
+++ Util/src/main/java/StringHelper.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 /**
  *  @author Julien Nadon
  *
@@ -5,8 +7,67 @@
  */
 public class StringHelper {
 
-    public static int someMethod(){
-        return 1;
+
+    public static String rightOf(String toSearchIn, String toSearchFor, int nChars, boolean trimResult) {
+
+        return null;
     }
+
+    public static String rightOf(String toSearchIn, String toSearchFor) {
+        return rightOf(toSearchIn, toSearchFor, 0, false);
+    }
+
+    public static String rightOf(String toSearchIn, String toSearchFor, int nChars){
+        return rightOf(toSearchIn, toSearchFor, nChars, false);
+    }
+
+    public static String rightOf(String toSearchIn, String toSearchFor, boolean trimResult){
+        return rightOf(toSearchIn, toSearchFor, 0, false    );
+    }
+
+    public static String leftOf(String toSearchIn, String toSearchFor, int nChars, boolean trimResult) {
+
+        return null;
+    }
+
+    public static String leftOf(String toSearchIn, String toSearchFor) {
+        return rightOf(toSearchIn, toSearchFor, 0, false);
+    }
+
+    public static String leftOf(String toSearchIn, String toSearchFor, int nChars){
+        return rightOf(toSearchIn, toSearchFor, nChars, false);
+    }
+
+    public static String leftOf(String toSearchIn, String toSearchFor, boolean trimResult){
+        return rightOf(toSearchIn, toSearchFor, 0, false);
+    }
+
+
+    public static String between(String toSaerchIn, String leftLimit, String rightLimit, boolean trimResults) {
+        return null;
+    }
+
+    public static String between(String toSaerchIn, String leftLimit, String rightLimit) {
+        return between(toSaerchIn, leftLimit, rightLimit, true);
+    }
+
+    public static String getFlatPhoneNumber(String phoneNumber){
+        return null;
+    }
+
+    public static String getFormatedPhoneNumber(String phoneNumber){
+        return null;
+    }
+
+    /*
+    @TODO Find a Java alternative for Xelement
+    public static String getIntValueFromXmlElement(List<Xelement> elementList, String elementName) {
+        return null;
+    }
+
+    public static BigDecimal GetDecimalValueFromXmlElement(List<Xelement> elementList, String elementName){
+        return null;
+    }
+    */
 
 }
--- Util/src/test/java/StringHelperTest.java
+++ Util/src/test/java/StringHelperTest.java
@@ -3,10 +3,66 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringHelperTest {
+    @Test
+    void rightOf() {
+        fail("TODO");
+    }
 
     @Test
-    void someMethod() {
-        assertEquals(1,StringHelper.someMethod());
+    void rightOf1() {
+        fail("TODO");
     }
+
+    @Test
+    void rightOf2() {
+        fail("TODO");
+    }
+
+    @Test
+    void rightOf3() {
+        fail("TODO");
+    }
+
+    @Test
+    void leftOf() {
+        fail("TODO");
+    }
+
+    @Test
+    void leftOf1() {
+        fail("TODO");
+    }
+
+    @Test
+    void leftOf2() {
+        fail("TODO");
+    }
+
+    @Test
+    void leftOf3() {
+        fail("TODO");
+    }
+
+    @Test
+    void between() {
+        fail("TODO");
+    }
+
+    @Test
+    void between1() {
+        fail("TODO");
+    }
+
+    @Test
+    void getFlatPhoneNumber() {
+        fail("TODO");
+    }
+
+    @Test
+    void getFormatedPhoneNumber() {
+        fail("TODO");
+    }
+
+
 
 }
