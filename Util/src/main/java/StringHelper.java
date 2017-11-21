32d0bebcc6b673f7a6e47927b9a9614933e22fd7
Nadon <jnadon7@gmail.com>
2017-11-21 13:17:48 -0800

    test case and implementation for between method

--- Util/src/main/java/StringHelper.java
+++ Util/src/main/java/StringHelper.java
@@ -1,21 +1,20 @@
-import java.math.BigDecimal;
-
 /**
- *  @author Julien Nadon
+ * @author Julien Nadon
  *
- *   StringHelper Class
+ * StringHelper Class
  */
 
 public class StringHelper {
 
     /**
-     *  Provides nChars located directly to the right of the first occurrence of toSearchFor.
-     * @param toSearchIn String to look in
+     * Provides nChars located directly to the right of the first occurrence of toSearchFor.
+     *
+     * @param toSearchIn  String to look in
      * @param toSearchFor String to look for
-     * @param nChars Maximum number of chars to return. If 0, return everything to the right of toSearchFor.
-     * @param trimResult whether or not to trim the results.
+     * @param nChars      Maximum number of chars to return. If 0, return everything to the right of toSearchFor.
+     * @param trimResult  whether or not to trim the results.
      * @return Returns string of maximum nChars found in toSearchIn, right of the first occurrence of toSearchFor
-     *  returns an empty string if toSearchFor is not found within toSearchIn
+     * returns an empty string if toSearchFor is not found within toSearchIn
      */
     public static String rightOf(String toSearchIn, String toSearchFor, int nChars, boolean trimResult) {
         if (toSearchFor == null || toSearchIn == null) return "";
@@ -27,7 +26,7 @@ public class StringHelper {
             nChars = toSearchIn.length() - firstO - toSearchFor.length();
         }
 
-        String res = toSearchIn.substring(firstO + toSearchFor.length() ,firstO + toSearchFor.length()+ nChars);
+        String res = toSearchIn.substring(firstO + toSearchFor.length(), firstO + toSearchFor.length() + nChars);
         if (trimResult) {
             res = res.trim();
         }
@@ -38,8 +37,8 @@ public class StringHelper {
 
     /**
      * Overloaded method with default param(s):
-     *     nChars=0
-     *     trimResult=true
+     * nChars=0
+     * trimResult=true
      */
     public static String rightOf(String toSearchIn, String toSearchFor) {
         return rightOf(toSearchIn, toSearchFor, 0, true);
@@ -47,28 +46,29 @@ public class StringHelper {
 
     /**
      * Overloaded method with default param(s):
-     *     trimResult=true
+     * trimResult=true
      */
-    public static String rightOf(String toSearchIn, String toSearchFor, int nChars){
+    public static String rightOf(String toSearchIn, String toSearchFor, int nChars) {
         return rightOf(toSearchIn, toSearchFor, nChars, true);
     }
 
     /**
      * Overloaded method with default param(s):
-     *     default nChars=0
+     * default nChars=0
      */
-    public static String rightOf(String toSearchIn, String toSearchFor, boolean trimResult){
-        return rightOf(toSearchIn, toSearchFor, 0, trimResult    );
+    public static String rightOf(String toSearchIn, String toSearchFor, boolean trimResult) {
+        return rightOf(toSearchIn, toSearchFor, 0, trimResult);
     }
 
     /**
-     *  Provides nChars located directly to the left of the first occurrence of toSearchFor.
-     * @param toSearchIn String to look in
+     * Provides nChars located directly to the left of the first occurrence of toSearchFor.
+     *
+     * @param toSearchIn  String to look in
      * @param toSearchFor String to look for
-     * @param nChars Maximum number of chars to return. If 0, return everything to the left of toSearchFor.
-     * @param trimResult whether or not to trim the results.
+     * @param nChars      Maximum number of chars to return. If 0, return everything to the left of toSearchFor.
+     * @param trimResult  whether or not to trim the results.
      * @return Returns string of maximum nChars found in toSearchIn, left of the first occurrence of toSearchFor
-     *  returns an empty string if toSearchFor is not found within toSearchIn
+     * returns an empty string if toSearchFor is not found within toSearchIn
      */
     public static String leftOf(String toSearchIn, String toSearchFor, int nChars, boolean trimResult) {
         if (toSearchFor == null || toSearchIn == null) return "";
@@ -90,8 +90,8 @@ public class StringHelper {
 
     /**
      * Overloaded method with default param(s):
-     *     nChars=0
-     *     trimResult=true
+     * nChars=0
+     * trimResult=true
      */
     public static String leftOf(String toSearchIn, String toSearchFor) {
         return leftOf(toSearchIn, toSearchFor, 0, true);
@@ -99,68 +99,84 @@ public class StringHelper {
 
     /**
      * Overloaded method with default param(s):
-     *     trimResult=true
+     * trimResult=true
      */
-    public static String leftOf(String toSearchIn, String toSearchFor, int nChars){
+    public static String leftOf(String toSearchIn, String toSearchFor, int nChars) {
         return leftOf(toSearchIn, toSearchFor, nChars, true);
     }
 
     /**
      * Overloaded method with default param(s):
-     *     nChars=0
+     * nChars=0
      */
-    public static String leftOf(String toSearchIn, String toSearchFor, boolean trimResult){
+    public static String leftOf(String toSearchIn, String toSearchFor, boolean trimResult) {
         return leftOf(toSearchIn, toSearchFor, 0, trimResult);
     }
 
     /**
-     *
-     * @param toSaerchIn String to search within
-     * @param leftLimit left delimiting value
+     * @param toSearchIn String to search within
+     * @param leftLimit  left delimiting value
      * @param rightLimit right delimiting value
      * @param trimResult whether or not to trim the results.
-     * @return provides a string of maximum nChars found in toSearchIn,  delimited at left by "leftLimit" and at the right side by "rightLimit"
+     * @return provides a string  delimited at left by "leftLimit" and at the right side by "rightLimit" found in toSearchIn
      */
-    public static String between(String toSaerchIn, String leftLimit, String rightLimit, boolean trimResult) {
-        return null;
+    public static String between(String toSearchIn, String leftLimit, String rightLimit, boolean trimResult) {
+        int right = -1;
+        int left = toSearchIn.indexOf(leftLimit);
+        if (rightLimit.equals(leftLimit)|| leftLimit.indexOf(rightLimit) >= 0){
+            right = toSearchIn.indexOf(rightLimit, left+leftLimit.length());
+        } else{
+            right = toSearchIn.indexOf(rightLimit);
+        }
+        if (left < 0 || right < 0){
+            return "";
+        }
+        String ret = toSearchIn.substring(left+leftLimit.length(),right);
+        if (trimResult){
+            return ret.trim();
+        } else {
+            return ret;
+        }
     }
 
     /**
      * Overloaded method with default param(s):
-     *     trimResult=0
+     * trimResult=0
      */
-    public static String between(String toSaerchIn, String leftLimit, String rightLimit) {
-        return between(toSaerchIn, leftLimit, rightLimit, true);
+    public static String between(String toSearchIn, String leftLimit, String rightLimit) {
+        return between(toSearchIn, leftLimit, rightLimit, true);
     }
 
     /**
      * From a formated phone number, reutrn the flat phone number (10digits)
+     *
      * @param phoneNumber the formated phone number (XXX)-XXX-XXXX
      * @return a flat phone number XXXXXXXXXXX
      */
-    public static String getFlatPhoneNumber(String phoneNumber){
-        String flat = phoneNumber.replaceAll("[^0-9.]","");
+    public static String getFlatPhoneNumber(String phoneNumber) {
+        String flat = phoneNumber.replaceAll("[^0-9.]", "");
         if (flat.length() != 10) {
-                return "";
+            return "";
         }
         return flat;
     }
 
     /**
      * From a flat phone number, return as a string the formatted phone number
+     *
      * @param phoneNumber a flat phone number XXXXXXXXXXX (10 dig)
      * @return a phone number with the format (XXX)-XXX-XXXX
      */
-    public static String getFormatedPhoneNumber(String phoneNumber){
+    public static String getFormatedPhoneNumber(String phoneNumber) {
         String seperator = "-";
         String formatedNumber = new StringBuilder()
                 .append("(")
-                .append(phoneNumber.substring(1,4))
+                .append(phoneNumber.substring(1, 4))
                 .append(")")
                 .append(seperator)
-                .append(phoneNumber.substring(4,7))
+                .append(phoneNumber.substring(4, 7))
                 .append(seperator)
-                .append(phoneNumber.substring(7,11))
+                .append(phoneNumber.substring(7, 11))
                 .toString();
         return formatedNumber;
     }
--- Util/src/test/java/StringHelperTest.java
+++ Util/src/test/java/StringHelperTest.java
@@ -1,77 +1,138 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringHelperTest {
     private final String testString = "test string 123bill Pay ";
 
-    @Test // normal case
+    @Test
+        // normal case
     void rightOf1() {
-        assertEquals("bill", StringHelper.rightOf(testString,"123",4, false));
+        assertEquals("bill", StringHelper.rightOf(testString, "123", 4, false));
     }
-    @Test // toSearchFor not found
+
+    @Test
+        // toSearchFor not found
     void rightOf2() {
-        assertEquals("", StringHelper.rightOf(  testString,"TTT",5,false));
+        assertEquals("", StringHelper.rightOf(testString, "TTT", 5, false));
     }
-    @Test // nChars = 0
+
+    @Test
+        // nChars = 0
     void rightOf3() {
-        assertEquals("bill Pay ", StringHelper.rightOf(testString,"123", 0,false));
+        assertEquals("bill Pay ", StringHelper.rightOf(testString, "123", 0, false));
     }
-    @Test // nChars != 0
+
+    @Test
+        // nChars != 0
     void rightOf4() {
-        assertEquals("bi", StringHelper.rightOf(testString,"123",2));
+        assertEquals("bi", StringHelper.rightOf(testString, "123", 2));
     }
-    @Test // trimResult = true
+
+    @Test
+        // trimResult = true
     void rightOf5() {
-        assertEquals("123bill Pay", StringHelper.rightOf(testString,"string",0, true));
+        assertEquals("123bill Pay", StringHelper.rightOf(testString, "string", 0, true));
     }
-    @Test // toSearchIn = null
+
+    @Test
+        // toSearchIn = null
     void rightOf6() {
-        assertEquals("", StringHelper.rightOf(null,"123",0));
+        assertEquals("", StringHelper.rightOf(null, "123", 0));
     }
-    @Test // toSearchFor = null
+
+    @Test
+        // toSearchFor = null
     void rightOf7() {
-        assertEquals("", StringHelper.rightOf(testString,null,0));
+        assertEquals("", StringHelper.rightOf(testString, null, 0));
     }
 
-    @Test // normal case
+    @Test
+        // normal case
     void leftOf1() {
-        assertEquals(" 123", StringHelper.leftOf(testString,"bill",4, false));
-    }
-    @Test // toSearchFor not found
-    void leftOf2() {
-        assertEquals("", StringHelper.leftOf(testString,"TTT",5,false));
-    }
-    @Test // nChars = 0
-    void leftOf3() {
-        assertEquals("test string ", StringHelper.leftOf(testString,"123", 0,false));
-    }
-    @Test // nChars != 0
-    void leftOf4() {
-        assertEquals("g ", StringHelper.leftOf(testString,"123",2, false));
-    }
-    @Test // trimResult = true
-    void leftOf5() {
-        assertEquals("test", StringHelper.leftOf(testString,"string",0, true));
-    }
-    @Test // toSearchIn = null
-    void leftOf6() {
-        assertEquals("", StringHelper.leftOf(null,"123",0));
-    }
-    @Test // toSearchFor = null
-    void leftOf7() {
-        assertEquals("", StringHelper.leftOf(testString,null,0));
+        assertEquals(" 123", StringHelper.leftOf(testString, "bill", 4, false));
     }
 
-    @Test // Valid number
+    @Test
+        // toSearchFor not found
+    void leftOf2() {
+        assertEquals("", StringHelper.leftOf(testString, "TTT", 5, false));
+    }
+
+    @Test
+        // nChars = 0
+    void leftOf3() {
+        assertEquals("test string ", StringHelper.leftOf(testString, "123", 0, false));
+    }
+
+    @Test
+        // nChars != 0
+    void leftOf4() {
+        assertEquals("g ", StringHelper.leftOf(testString, "123", 2, false));
+    }
+
+    @Test
+        // trimResult = true
+    void leftOf5() {
+        assertEquals("test", StringHelper.leftOf(testString, "string", 0, true));
+    }
+
+    @Test
+        // toSearchIn = null
+    void leftOf6() {
+        assertEquals("", StringHelper.leftOf(null, "123", 0));
+    }
+
+    @Test
+        // toSearchFor = null
+    void leftOf7() {
+        assertEquals("", StringHelper.leftOf(testString, null, 0));
+    }
+
+    @Test
+        // Valid case
+    void between() {
+        assertEquals("b", StringHelper.between("abc", "a", "c"));
+    }
+
+    @Test
+        // left limit not found
+    void between2() {
+        assertEquals("", StringHelper.between("abc", "z", "c"));
+    }
+
+    @Test
+        // left limit not found
+    void between3() {
+        assertEquals("", StringHelper.between("abc", "a", "d"));
+    }
+
+    @Test
+        // leftLimit == rightLimit
+    void between4() {
+        assertEquals("test", StringHelper.between(",1,1test,1", ",1,1", ",1"));
+    }
+
+    @Test
+        // trimResult == true
+    void between5() {
+        assertEquals("is a", StringHelper.between("this is a test", "this", "test", true));
+    }
+
+    @Test
+        // Valid number
     void getFlatPhoneNumber() {
         assertEquals("1231231234", StringHelper.getFlatPhoneNumber("(123)-123-1234"));
     }
-    @Test // invalid number (too long)
+
+    @Test
+        // invalid number (too long)
     void getFlatPhoneNumber2() {
         assertEquals("", StringHelper.getFlatPhoneNumber("(123)-123-123456"));
     }
-    @Test // invalid number (too short)
+
+    @Test
+        // invalid number (too short)
     void getFlatPhoneNumber3() {
         assertEquals("", StringHelper.getFlatPhoneNumber("(123)-123-12"));
     }
